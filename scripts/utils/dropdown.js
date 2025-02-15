a4701193c47b46867e37675d9df074e17abf025f
HOELCODE <hcostantini1@gmail.com>
2025-02-15 23:37:25 +0100

    dropdown

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -1,15 +1,33 @@
+//fonction pour fermer dropdown
+const closeDropdown = (button, div, number) => {
+        button.classList.toggle('fa-chevron-up');
+        button.classList.add('fa-chevron-down');
+        button.classList.toggle('open');
+        div[number].classList.add('hidden');
+}
+
+//fonction pour ouvrir dropdown
+const openDropdown = (button, div, number) => {
+        button.classList.add('fa-chevron-up');
+        button.classList.toggle('fa-chevron-down');
+        button.classList.add('open');
+        div[number].classList.toggle('hidden');
+}
+
 //fonction pour ouvrir et fermer le dropdown
 const dropdown = () => {
-    const dropdown = document.querySelector('.dropdown');
-    const dropdownContent = document.querySelector('.dropdown-content');
-
-    dropdown.addEventListener('click', () => {
-        if (dropdownContent.classList.contains('dropdown-content-open')) {
-            dropdownContent.classList.remove('dropdown-content-open');
-            dropdownContent.classList.add('dropdown-content-close');
+    const dropdown = document.querySelectorAll('.dropdown');
+    const dropdownContent = document.querySelectorAll('.dropdown-content');
+    
+    dropdown.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        if (dropdownContent[index].classList.contains('hidden')) {
+            openDropdown(element, dropdownContent, index);
         } else {
-            dropdownContent.classList.remove('dropdown-content-close');
-            dropdownContent.classList.add('dropdown-content-open');
+            closeDropdown(element, dropdownContent, index);
         }
     });
-}   
+});
+}
+
+dropdown();
