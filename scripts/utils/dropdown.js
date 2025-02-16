b2fdb2e904fe8f05dd86feafd17612770654ddcf
HOELCODE <hcostantini1@gmail.com>
2025-02-16 16:42:11 +0100

    dropdown input

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -1,15 +1,15 @@
 //fonction pour fermer dropdown
-const closeDropdown = (button, div, number) => {
-        button.classList.toggle('fa-chevron-up');
-        button.classList.add('fa-chevron-down');
+const closeDropdown = (button, div, chevron, number) => {
+        chevron[number].classList.toggle('fa-chevron-up');
+        chevron[number].classList.add('fa-chevron-down');
         button.classList.toggle('open');
         div[number].classList.add('hidden');
 }
 
 //fonction pour ouvrir dropdown
-const openDropdown = (button, div, number) => {
-        button.classList.add('fa-chevron-up');
-        button.classList.toggle('fa-chevron-down');
+const openDropdown = (button, div, chevron, number) => {
+        chevron[number].classList.add('fa-chevron-up');
+        chevron[number].classList.toggle('fa-chevron-down');
         button.classList.add('open');
         div[number].classList.toggle('hidden');
 }
@@ -17,17 +17,20 @@ const openDropdown = (button, div, number) => {
 //fonction pour ouvrir et fermer le dropdown
 const dropdown = () => {
     const dropdown = document.querySelectorAll('.dropdown');
+    const chevron = document.querySelectorAll('.dropdown i');
     const dropdownContent = document.querySelectorAll('.dropdown-content');
     
     dropdown.forEach((element, index) => {
     element.addEventListener('click', () => {
         if (dropdownContent[index].classList.contains('hidden')) {
-            openDropdown(element, dropdownContent, index);
+            openDropdown(element, dropdownContent, chevron, index);
         } else {
-            closeDropdown(element, dropdownContent, index);
+            closeDropdown(element, dropdownContent, chevron, index);
         }
     });
 });
 }
+
+// Déclaration des fonctions
 
 dropdown();
---
+++ scripts/utils/input.js
@@ -0,0 +1,32 @@
+// Fonction pour afficher la croix quand on commence à écrire dans le input
+const showCross = () => {
+    const input = document.querySelectorAll('input');
+    const cross = document.querySelectorAll('.fa-xmark');
+
+    input.forEach((element, index) => {
+        element.addEventListener('input', () => {
+            if (element.value.length > 0) {
+                cross[index].classList.remove('hidden');
+            } else {
+                cross[index].classList.add('hidden');
+            }
+        });
+    });
+}
+
+// Fonction pour effacer le contenu du input
+const clearInput = () => {
+    const input = document.querySelectorAll('input');
+    const cross = document.querySelectorAll('.fa-xmark');
+
+    cross.forEach((element, index) => {
+        element.addEventListener('click', () => {
+            input[index].value = '';
+            element.classList.add('hidden');
+        });
+    });
+}
+
+// Déclaration des fonctions
+clearInput();
+showCross();
