3904abdf4d1c852db67f0434af5881bb65690231
HOELCODE <hcostantini1@gmail.com>
2025-03-02 17:28:49 +0100

    80%

--- scripts/controllers/DataController.js
+++ scripts/controllers/DataController.js
@@ -1,6 +1,7 @@
 import DataModel from "../models/DataModel.js";
 import DataView from "../views/DataView.js";
 import { searchRecipes } from "../utils/mainSearch.js";
+import { selectedTags } from "../search/addTag.js";
 
 class DataController {
     constructor() {
@@ -8,16 +9,32 @@ class DataController {
         this.view = new DataView();
         // Nouveaux tableaux
         this.filteredRecipes = [];
+        this.lastFilteredRecipes = [];
     }
 
     async init() {
         await this.model.loadData();
         // Recettes
         this.loadRecipeView(this.model.getAllRecipes());
-        // Update Recettes view
+        // Mettre à jour les recettes lorsqu'une recherche est fait sur le main search
         document.querySelector(".input-search").addEventListener("input", (event) => {
             this.handleMainInputSearch(event.target.value);
         });
+        // Mettre à jour les recettes et les filtres lorsqu'un tag est ajouté
+        document.addEventListener("tagsAdded", () => {
+            if (this.filteredRecipes.length > 0) {
+            this.handleTagFilter(this.filteredRecipes, selectedTags.map(tag => tag.name).join(" "));
+            } else {
+            this.handleTagFilter(this.model.getAllRecipes(), selectedTags.map(tag => tag.name).join(" "));
+            }
+        });
+        // Mettre à jour les recettes et les filtres lorsqu'un tag est supprimé
+        document.addEventListener("tagsDeleted", () => {
+            this.filteredRecipes = this.lastFilteredRecipes;
+            this.updateRecipesView(this.lastFilteredRecipes);
+            this.loadComponentsView(this.model.getAllComponents(this.lastFilteredRecipes));
+        });
+
         // Composants
         this.loadComponentsView(this.model.getAllComponents(this.model.getAllRecipes()));
     }
@@ -28,13 +45,22 @@ class DataController {
         this.view.displayRecipes(recipes);
     }
 
-    handleMainInputSearch (query) {
+    handleMainInputSearch(query) {
         const allRecipes = this.model.getAllRecipes();
         this.filteredRecipes = searchRecipes(allRecipes, query);
         // Affichage des nouvelles recettes
         this.updateRecipesView(this.filteredRecipes);
         // Affichage des nouveaux composants
         this.loadComponentsView(this.model.getAllComponents(this.filteredRecipes));
+    }
+
+    handleTagFilter(liste, query) {
+        this.lastFilteredRecipes = liste;
+        liste = searchRecipes(liste, query);
+        // Affichage des nouvelles recettes
+        this.updateRecipesView(liste);
+        // Affichage des nouveaux composants
+        this.loadComponentsView(this.model.getAllComponents(liste));
     }
 
     updateRecipesView(recipes) {
--- scripts/search/addTag.js
+++ scripts/search/addTag.js
@@ -15,12 +15,37 @@ const addTag = () => {
             if (!selectedTags.some(t => t.name === tag)) {
                 selectedTags.push({ name: tag, class: tagClass });
                 updateTagsDisplay();
+                // Dispatch event
+                document.dispatchEvent(new Event("tagsAdded"));
             }
         }
         
     });
 }
 
+// Fonction pour supprimer un tag de la liste selectedTags
+const deleteTag = () => {
+    const container = document.querySelector('.tags-container');
+
+    container.addEventListener('click', (event) => {
+        if (event.target.tagName === 'I') {
+            const element = event.target;
+            const divElement = element.parentElement;
+            const spanElement = element.previousElementSibling;
+            const tag = spanElement.textContent;
+            const tagClass = spanElement.className;
+            
+            // Supprimer le tag du tableau SelectedTags
+            selectedTags = selectedTags.filter(t => t.name !== tag);
+            // Ajouter le tag à la liste deletedTags
+            DeleteTagDisplay(divElement);
+            // Dispatch event
+            document.dispatchEvent(new Event("tagsDeleted"));
+        }
+    });
+}
+
+// Fonction pour afficher un tag sélectionné
 const updateTagsDisplay = () => {
     const tagsContainer = document.querySelector('.tags-container');
     tagsContainer.innerHTML = ""; // Vider l'affichage actuel
@@ -34,20 +59,13 @@ const updateTagsDisplay = () => {
         `;
         tagsContainer.appendChild(divTag);
     });
-
-    updateFilterList();
 }
 
-// Fonction pour mettre à jour la liste des filtres
-const updateFilterList = () => {
-    const ulLists = document.querySelectorAll(".dropdown-list");
+// Fonction pour supprimer l'affichage du Tag
+const DeleteTagDisplay = (element) => {
+    element.remove();
+}
 
-    ulLists.forEach(ul => {
-        Array.from(ul.children).forEach(li => {
-            const isSelected = selectedTags.some(tag => tag.name === li.textContent);
-            li.style.display = isSelected ? "none" : "";
-        });
-    });
-};
-
+// Déclaration des fonctions
 document.addEventListener('recipeLoaded', addTag());
+deleteTag();
